b6f691bc05640bfabaf49acd0e4e62939b390ab8
Brandon Perard <brandon.perard@gmail>
2021-01-14 14:44:59 -0800

    update utility functions to pass tests for all outcomes, update index.html to add img for throw outcomes user & comp, complete game functionality for app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-
+import { getRandomThrow } from './get-random-throw.js';
+import { didUserWin } from './utils.js';
 
 // import functions and grab DOM elements
 const confirmButton = document.getElementById('confirm-button');
@@ -7,19 +7,40 @@ const youWon = document.getElementById('you-won-secret-div');
 const secretWins = document.getElementById('secret-wins');
 const secretLosses = document.getElementById('secret-losses');
 const secretTotal = document.getElementById('secret-total');
-let rng = Math.ceil(Math.random() * 3);
+const userThrowImg = document.querySelector('#user-throw-img');
+const computerThrowImg = document.querySelector('#computer-throw-img');
 // initialize state
 let wins = 0;
 let losses = 0;
 let total = 0;
-
-// set event listeners to update state and DOM
+const paper = 'https://nyc3.digitaloceanspaces.com/aph/app/uploads/2019/04/26160704/1-04851-00_BL_Notebook_Paper_Punch_G-600x735';
+const rock = 'https://static.toiimg.com/thumb/msid-76750498,imgsize-965136,width-800,height-600,resizemode-75/76750498';
+const scissors = 'https://images-na.ssl-images-amazon.com/images/I/61vNqndZyJL._AC_SL1280_'; https://images-na.ssl-images-amazon.com/images/I/61vNqndZyJL._AC_SL1280_
 
 confirmButton.addEventListener('click', () => {
-    total++;
+    const userInput = document.querySelector('input:checked');
+    const userThrow = userInput.value;
+    const computerThrow = getRandomThrow();
+    const result = didUserWin(userThrow, computerThrow);
+    userThrowImg.src = `/${userThrow}.jpg`;
+    //userThrowImg.src = userInput.value;
+    // console.log(userThrow);
+    // // let computerThrowImg = `${computerThrow}.jpg`;
+    // // computerThrowImg.src = computerThrowImg;
+    if (userThrow === 'paper') {
+        userThrowImg.src = `${paper}.jpg`;
+    } else if (userThrow === 'rock') {
+        userThrowImg.src = `${rock}.jpg`;
+    } else {
+        userThrowImg.src = `${scissors}.jpg`;
+    }
+    if (computerThrow === 'paper') {
+        computerThrowImg.src = `${paper}.jpg`;
+    } else if (computerThrow === 'rock') {
+        computerThrowImg.src = `${rock}.jpg`;
+    } else {
+        computerThrowImg.src = `${scissors}.jpg`;
+    }
 
-    
 
-   
-    secretTotal.textContent = total;
 }); 
